cff41f429122d9309507392a0df7b37d39e80d53
pav_nk <pav_nk@vk.com>
2024-12-07 16:51:31 +0300

    feat: 44 tasks completed

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -352,8 +352,15 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return arr.reduce((acc, el, idx) => {
+    const chunkIdx = Math.floor(idx / chunkSize);
+    if (!acc[chunkIdx]) {
+      acc[chunkIdx] = [];
+    }
+    acc[chunkIdx].push(el);
+    return acc;
+  }, []);
 }
 
 /**
@@ -394,8 +401,11 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  return indices.reduce((acc, currentIdx) => {
+    const newAcc = acc[currentIdx];
+    return newAcc;
+  }, arr);
 }
 
 /**
